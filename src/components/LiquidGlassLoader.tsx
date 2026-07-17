6ead461415efb28136c80f1126976188fc2a3740
cyxysky <18367692531@163.com>
2026-07-17 14:28:27 +0800

    优化样式

--- src/components/LiquidGlassLoader.tsx
+++ src/components/LiquidGlassLoader.tsx
@@ -12,7 +12,6 @@ export function LiquidGlassLoader({ className = '' }: LiquidGlassLoaderProps) {
         <i className="ui-liquid-glass-bubble" style={{ '--x': '34%', '--delay': '-.2s' } as CSSProperties} />
         <i className="ui-liquid-glass-bubble ui-liquid-glass-bubble--small" style={{ '--x': '57%', '--delay': '-1.4s' } as CSSProperties} />
         <i className="ui-liquid-glass-bubble ui-liquid-glass-bubble--large" style={{ '--x': '69%', '--delay': '-2.1s' } as CSSProperties} />
-        <span className="ui-liquid-glass-glint" />
       </span>
     </span>
   );
